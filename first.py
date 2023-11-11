6facde41c16d2a452f7e7ac87c9f1909575b6545
Hanu-unique <hanugupta456@gmail.com>
2023-11-11 12:21:58 +0530

    recent commit

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
-if a == b: 
+if a == b: #if statement
     print("Both numbers are equal")
 elif a > b:
     print("First number is greater than second number")
